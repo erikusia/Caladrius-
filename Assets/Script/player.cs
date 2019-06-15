27bebf5dd0bc0f99faaeb37f768127658915e3fd
kaede828 <kaede1999828@gmail.com>
2019-06-15 11:25:20 +0900

    player動き追加

---
+++ Assets/Script/player.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player : MonoBehaviour
+{
+    public float speed = 5f;
+    float MoveX = 0f;
+    float MoveZ = 0f;
+
+    public Rigidbody rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MoveX = Input.GetAxisRaw("Horizontal") * speed;
+        MoveZ = Input.GetAxisRaw("Vertical") * speed;
+        Vector3 direction = new Vector3(MoveX, 0, MoveZ);
+        rb.constraints = RigidbodyConstraints.FreezePositionY;
+    }
+
+    void FixedUpdate()
+    {
+        rb.velocity = new Vector3(MoveX, 0, MoveZ);
+    }
+}
