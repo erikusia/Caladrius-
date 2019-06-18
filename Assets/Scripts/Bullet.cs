f1c5d12a94e92862ea74a1a3d0da3250f7c11382
erikusia <erikusiamk2@gmail.com>
2019-06-18 09:51:48 +0900

    enemy1を追加

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public float BulletSpeed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<Rigidbody>().velocity = transform.forward.normalized * BulletSpeed;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!GetComponent<Renderer>().isVisible)
+        {
+            //Debug.Log("画面外です");
+            //Destroy(gameObject);
+        }
+    }
+
+    private void OnBecameInvisible()
+    {
+        Debug.Log("画面外です");
+
+        Rect rc = new Rect(Vector2.zero, new Vector2(1000, 5000));
+        rc.Contains()
+    }
+}
---
+++ Assets/Scripts/NewBehaviourScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    // 移動スピード
+    public float speed = 5;
+
+    void Update()
+    {
+        // 右・左
+        float x = Input.GetAxisRaw("Horizontal");
+
+        // 上・下
+        float z = Input.GetAxisRaw("Horizontal");
+
+        // 移動する向きを求める
+        Vector2 direction = new Vector3(x,0, z).normalized;
+
+        // 移動する向きとスピードを代入する
+        GetComponent<Rigidbody>().velocity = direction * speed;
+    }
+}
