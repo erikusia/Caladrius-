3349ffd0852f67249c03d6d7871ec2939c3e3bb4
erikusia <erikusiamk2@gmail.com>
2019-06-18 16:08:56 +0900

    Enemy1を追加

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -8,24 +8,7 @@ public class Bullet : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        GetComponent<Rigidbody>().velocity = transform.forward.normalized * BulletSpeed;
+        GetComponent<Rigidbody>().velocity = transform.forward* BulletSpeed;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        if (!GetComponent<Renderer>().isVisible)
-        {
-            //Debug.Log("画面外です");
-            //Destroy(gameObject);
-        }
-    }
-
-    private void OnBecameInvisible()
-    {
-        Debug.Log("画面外です");
-
-        Rect rc = new Rect(Vector2.zero, new Vector2(1000, 5000));
-        rc.Contains()
-    }
 }
