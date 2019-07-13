98a6ac050bac87d7dd2ca3a7f780a1779d11a43e
erikusia <erikusiamk2@gmail.com>
2019-07-13 14:00:57 +0900

    特殊2にSEを追加

--- Assets/Scripts/Gage2.cs
+++ Assets/Scripts/Gage2.cs
@@ -3,43 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+
 public class Gage2 : MonoBehaviour
 {
-    //Slider slider;
-    //float gage = 100;
-    //private int timeCount;
-
-    //// Start is called before the first frame update
-    //void Start()
-    //{
-    //    slider = GameObject.Find("ZSlider2").GetComponent<Slider>();
-    //}
-
-    //// Update is called once per frame
-    //void Update()
-    //{
-
-    //    timeCount += 1;
-    //    if (Input.GetKey(KeyCode.F)|| Input.GetButton("Ybutton"))
-    //    {
-    //        gage -= 10f * Time.deltaTime;
-
-    //        gage = Mathf.Max(gage - 10.0f * Time.deltaTime, 0);
-    //    }
-
-    //    if (timeCount % 10 == 0)
-    //    {
-    //        gage += 2.0f * Time.deltaTime;
-
-    //        gage = Mathf.Min(gage + 2.0f * Time.deltaTime, 100);
-    //    }
-
-    //    //Debug.Log(gage);
-
-    //    gage = Mathf.Clamp(gage, 0, 100);
-    //    slider.value = gage;
-    //}
-
     Slider slider;
     float gage = 100;
     private int timeCount;
@@ -48,6 +14,9 @@ public class Gage2 : MonoBehaviour
     [SerializeField]
     float shotinterval;
 
+    public AudioClip sound1;
+    AudioSource audioSource;
+
 
     // Start is called before the first frame update
     void Start()
@@ -55,6 +24,7 @@ public class Gage2 : MonoBehaviour
         character = GetComponent<Character>();
         slider = GameObject.Find("ZSlider2").GetComponent<Slider>();
         StartCoroutine(PlayerShot());
+        audioSource = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -87,6 +57,7 @@ public class Gage2 : MonoBehaviour
                 {
                     Debug.Log("撃てません！");
                 }
+                audioSource.PlayOneShot(sound1);
 
             }
 
