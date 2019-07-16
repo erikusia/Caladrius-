d01904b9b9da240122c9a02b090c2f1f74749e64
erikusia <erikusiamk2@gmail.com>
2019-07-16 13:10:20 +0900

    わかんね

--- Assets/Scripts/Gage3.cs
+++ Assets/Scripts/Gage3.cs
@@ -6,6 +6,7 @@ using InputKey;
 
 public class Gage3 : MonoBehaviour
 {
+<<<<<<< HEAD
     //Slider slider;
     //float gage = 100;
     //private int timeCount;
@@ -134,4 +135,85 @@ public class Gage3 : MonoBehaviour
     //        yield return null;
     //    }
     //}
+=======
+    Slider slider;
+    float gage = 100;
+    private int timeCount;
+    Character character;
+    ParticleSystem particle;
+    Collider collider;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        character = GetComponent<Character>();
+        slider = GameObject.Find("ZSlider3").GetComponent<Slider>();
+        
+        particle = GameObject.Find("PlayerShield 1").GetComponent<ParticleSystem>();
+        collider = GameObject.Find("PlayerShield 1").GetComponent<Collider>();
+        collider.enabled = false;
+
+        StartCoroutine(PlayerShot());
+        //Debug.Log(particle);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    IEnumerator PlayerShot()
+    {
+        while (true)
+        {
+            
+            if (MyInput.MyInputKey(KeyCode.E) || MyInput.MyInputButton("Bbutton"))
+            {
+               if( particle.isPlaying  == false)
+                {
+                    particle.Play(true);
+                    collider.enabled = true;
+                }
+                if (gage > 0)
+                {
+
+                    for (int i = 0; i < 3; i++)
+                    {
+                        Transform shotPosition = transform.GetChild(i);
+                        Debug.Log("出てます");
+                        //shotPositionの位置方向で
+                        character.Shot(shotPosition);
+                    }
+                    gage -= 1f;
+                    //Debug.Log(gage);
+                    yield return null;
+                }
+                else if (gage <= 0)
+                {
+                    Debug.Log("撃てません！");
+                }
+
+            }
+            else 
+            {
+                if (particle.isPlaying)
+                {
+                    particle.Stop(true);
+                    collider.enabled = false;
+                }
+
+            }
+
+            if (timeCount % 10 == 0)
+            {
+                gage += 2.0f * Time.deltaTime;
+            }
+
+            gage = Mathf.Clamp(gage, 0, 100);
+            slider.value = gage;
+
+            yield return null;
+        }
+    }
+>>>>>>> 2d784a6... UIを変更
 }
---
+++ Assets/Scripts/InputCheck.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace InputKey
+{
+    /// <summary>
+    /// 同時入力を禁止する
+    /// </summary>
+    public static class MyInput
+    {
+        static string isCheck_Input;
+
+        public static bool MyInputKey(KeyCode key)
+        {
+            //if (Input.anyKey == false) isCheck_Input = false;
+
+            //if (isCheck_Input == false)
+            //{
+            //    if (Input.GetKey(key))
+            //    {
+            //        isCheck_Input = true;
+            //        return true;
+            //    }
+            //}
+            return false;
+        }
+
+        public static bool MyInputButton(string key)
+        {
+            if (Input.anyKey == false) isCheck_Input = null;
+            if (isCheck_Input == null)
+            {
+                if(Input.GetButton(key))
+                {
+                    isCheck_Input = key;
+                    Debug.Log(isCheck_Input);               
+                    if (Input.GetButton(key)==Input.GetButton(isCheck_Input))
+                    {
+                        return false;
+                    }
+                    return true;
+
+                }
+                
+            }
+            return false;
+
+        }
+    }
+}
