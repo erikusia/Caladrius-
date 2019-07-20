d627ec91dc8aba75d8ac2f3efbdfa40b7fb032d6
kaede828 <kaede1999828@gmail.com>
2019-07-20 15:13:36 +0900

    no message

--- Assets/Scripts/sceneManager/TitleManager.cs
+++ Assets/Scripts/sceneManager/TitleManager.cs
@@ -17,7 +17,7 @@ public class TitleManager : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.Space)||Input.GetButton("Abutton"))
         {
-            SceneManager.LoadScene("Hit");
+            SceneManager.LoadScene("GamePlay");
         }
     }
 }
